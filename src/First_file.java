6fee7dbfc3fee502db49cd5576ff5b0099b99823
Abdullah-Malik012 <95423490+Abdullah-Malik012@users.noreply.github.com>
2021-12-02 20:26:11 +0500

    conflict

--- src/First_file.java
+++ src/First_file.java
@@ -4,6 +4,7 @@ public class First_file {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String name = "Abdullah Malik";
+		String name= "Abdullah";
 		String rollno = "20i-0930";
 		System.out.println("Name: " + name);
 		System.out.println("RollNo: " + rollno);
