fa9bb38441f3b931d0d7dc693da57cf1a054a705
Abdullah-Malik012 <95423490+Abdullah-Malik012@users.noreply.github.com>
2021-12-02 19:42:46 +0500

    my first commit

--- src/First_file.java
+++ src/First_file.java
@@ -3,8 +3,8 @@ public class First_file {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String name = "Abdullah Malik";
-		String rollno = "20i-0930";
+		String name = "Momin";
+		String rollno = "18i-0574";
 		System.out.println("Name: " + name);
 		System.out.println("RollNo: " + rollno);
 		
