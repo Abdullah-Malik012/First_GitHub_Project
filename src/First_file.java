5fa7196ae50a641588fe8ade86b9a6bbe875105f
hamza442-ali <71401547+hamza442-ali@users.noreply.github.com>
2021-12-03 06:15:22 -0800

    Update First_file.java
    My name is better

--- src/First_file.java
+++ src/First_file.java
@@ -3,8 +3,8 @@ public class First_file {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String name = "Abdullah Malik";
-		String rollno = "20i-0930";
+		String name = "Ali Hamza";
+		String rollno = "20i-1881";
 		System.out.println("Name: " + name);
 		System.out.println("RollNo: " + rollno);
 		
